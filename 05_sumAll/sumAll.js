1b8eded5ad86179fc833cb01b66952a5e100217c
Darron Dai <daidarron@gmail.com>
2024-05-13 17:19:14 -0700

    Complete ex5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  const low = Math.min(num1, num2);
+  const high = Math.max(num1, num2);
+  let sum = 0;
+  for (let val = low; val <= high; val++) {
+    sum += val;
+  }
+  return sum;
 };
 
 // Do not edit below this line
