e3340a9037cb072cf0885fb620a559935762952f
Darron Dai <daidarron@gmail.com>
2024-05-17 16:15:23 -0700

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+  const birth = person.yearOfBirth;
+  let death = person.yearOfDeath;
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currPerson) =>
+    getAge(oldest) < getAge(currPerson) ? currPerson : oldest
+  );
 };
 
 // Do not edit below this line
