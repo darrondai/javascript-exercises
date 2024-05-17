ee131801e69c343e8c2650e3695be35f1b2f3572
Darron Dai <daidarron@gmail.com>
2024-05-17 15:15:49 -0700

    Complete palidrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // preprocess to lowercase and strip punctuation and spaces
+  let nonalphanumeric = /\W/g;
+  let processed = string.toLowerCase().replace(nonalphanumeric, "");
+  console.log(processed);
 
+  // two pointers
+  let right = processed.length - 1;
+  for (let left = 0; left < right; left++) {
+    if (processed[left] !== processed[right]) {
+      return false;
+    }
+    right--;
+  }
+  return true;
 };
 
 // Do not edit below this line
